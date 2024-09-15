1953a8f0aff41f9c20f6e897ca9699e79e40ec53
草をこして森 <kyou0317kyou0317@gmail.com>
2024-09-15 21:37:45 +0900

    イメージが固まる前

--- script.jsx
+++ script.jsx
@@ -1,5 +1,24 @@
 const App = () => {
-  return <div className="hello">Hello, React!</div>;
+  return (
+    <div>
+      <div className="main_title">パイナップル積み上げ作業</div>
+      {/* 分けたコンポーネントをここで呼び出す */}
+      <WorkDescription />
+    </div>
+  );
+};
+
+// 仕事説明のコンポーネント
+const WorkDescription = () => {
+  return (
+    <div>
+      <h1>こんな仕事もあります</h1>
+      <img
+        src="./images/pineapple_girl.png"
+        alt="パイナップルガールのイラスト"
+      />
+    </div>
+  );
 };
 
 // Reactアプリを描画
