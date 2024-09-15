67e960777f92d25100073ba33e6cf6c87448750f
草をこして森 <kyou0317kyou0317@gmail.com>
2024-09-16 01:04:33 +0900

    ある程度内したい所は直した

--- script.jsx
+++ script.jsx
@@ -1,11 +1,23 @@
 const App = () => {
   return (
     <div>
-      <div className="main_title">パイナップル積み上げ作業</div>
-      {/* 分けたコンポーネントをここで呼び出す */}
+      <TitlePineapple />
+      <div className="container">
+        {/* 分けたコンポーネントをここで呼び出す */}
+        <Content_box />
+        <Content_box />
+      </div>
       <WorkDescription />
     </div>
   );
+};
+
+const TitlePineapple = () => {
+  return <div className="main_title">パイナップル積み上げ作業</div>;
+};
+
+const Content_box = () => {
+  return <div className="box"></div>;
 };
 
 // 仕事説明のコンポーネント
@@ -16,6 +28,7 @@ const WorkDescription = () => {
       <img
         src="./images/pineapple_girl.png"
         alt="パイナップルガールのイラスト"
+        className="pineapple_girl_image"
       />
     </div>
   );
